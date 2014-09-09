4eef9c13745db3da74c22a26b659dc054aec5500
Jonathan Reem <jonathan.reem@gmail.com>
2014-09-08 20:13:15 -0700

    (feat) MoveAcceptor trait.

---
+++ README.md
@@ -0,0 +1,6 @@
+# MoveAcceptor
+
+> A variant of the Acceptor trait which moves self in the `move_incoming` method.
+
+This produces an Iterator which fulfills 'static, which is sometimes useful.
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,33 @@
 #![deny(missing_doc)]
 #![deny(warnings)]
 
-//! Crate comment goes here
+//! A variant of the Acceptor trait which moves self in the `move_incoming` method.
+//!
+//! This creates an iterator which fulfills 'static.
+
+use std::io::{Acceptor, IoResult};
+
+/// A variant of the Acceptor trait which moves self in the `move_incoming` method.
+pub trait MoveAcceptor<T>: Acceptor<T> {
+    /// Wait for and accept an incoming connection.
+    fn maccept(&mut self) -> IoResult<T> { self.accept() }
+
+    /// Get an Iterator over incoming connections.
+    fn move_incoming(self) -> MoveConnections<Self> {
+        MoveConnections { underlying: self }
+    }
+}
+
+/// An iterator over incoming connections.
+pub struct MoveConnections<T> {
+    underlying: T
+}
+
+impl<T, A: Acceptor<T>> Iterator<IoResult<T>> for MoveConnections<A> {
+    fn next(&mut self) -> Option<IoResult<T>> {
+        Some(self.underlying.accept())
+    }
+}
+
+impl<A, T: Acceptor<A>> MoveAcceptor<A> for T {}
 
